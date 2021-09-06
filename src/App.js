a1e6ecdd9b14d5ea4ca603072a8b9af3c4b84808
doongseal <doongseal@naver.com>
2021-09-06 15:36:29 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routers/Home";
+import About from "./routers/About";
+import Detail from "./routers/Detail";
+import Navigation from "./components/Navigation";
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/movie/:id" component={Detail} />
+
+
+  </HashRouter>);
 }
 
 export default App;
---
+++ src/routers/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+
+
+    return <span> Wya hooooooooooooooooooooooo </span>
+}
+
+export default About
